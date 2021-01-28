fd497559c870bcfdd6c1c86c2cb4e02c906fbb16
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2021-01-28 08:48:21 -0800

    constructor (basic, no validation)

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,12 +1,16 @@
 public class Contacts
 {
     //variables
-
     private String name; //Must be all letters
     private String number;//in format (xxx)xxx-xxxx
     private String email;//in format (any amount of chars)@(any amount of chars).com
     //constructor
-
+    public Contacts(String name, String number, String email)
+    {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+    }//end constructor
     //getters/setters
 
     //brain methods - validation
