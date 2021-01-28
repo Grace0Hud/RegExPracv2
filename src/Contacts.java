0569bca04014792b33fde1544b52f451a91ad64b
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2021-01-28 08:46:05 -0800

    instance variables

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,3 +1,15 @@
 public class Contacts
 {
+    //variables
+
+    private String name; //Must be all letters
+    private String number;//in format (xxx)xxx-xxxx
+    private String email;//in format (any amount of chars)@(any amount of chars).com
+    //constructor
+
+    //getters/setters
+
+    //brain methods - validation
+
+    //tostring
 }//end contacts class
