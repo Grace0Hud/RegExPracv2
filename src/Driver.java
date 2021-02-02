10d762e415244b5bc3f8eacf9ab9f4131abfb0af
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2021-02-01 17:11:30 -0800

    email testing

--- src/Driver.java
+++ src/Driver.java
@@ -17,9 +17,7 @@ public class Driver
         System.out.println("FIRST NAME set to: " + potter.getFirstName());
         System.out.println("------------------------------");
 
-         */
 
-        /*
         //testing last name validation
         System.out.println("Tried setting LAST NAME to: ");
         System.out.println("\"potter\"");
@@ -31,7 +29,6 @@ public class Driver
         System.out.println("LAST NAME set to: " + potter.getLastName());
         System.out.println("------------------------------");
 
-         */
 
         //testing phone numbers
         System.out.println("Tried setting NUMBER to: ");
@@ -42,6 +39,20 @@ public class Driver
         System.out.println("\"nine\"");
         potter.setNumber("nine");
         System.out.println("NUMBER set to: " + potter.getNumber());
+        System.out.println("------------------------------");
+
+         */
+
+        System.out.println("Tried setting EMAIL to: ");
+        System.out.println("\"potter\"");
+        potter.setEmail("potter");
+        System.out.println("\"potter@\"");
+        potter.setEmail("potter@");
+        System.out.println("\"potter@potter.\"");
+        potter.setEmail("potter@potter.");
+        System.out.println("\"potter@potter.abc\"");
+        potter.setEmail("potter@potter.abc");
+        System.out.println("EMAIL set to: " + potter.getEmail());
         System.out.println("------------------------------");
     }//end main
 }//end class driver
