77892ad93a10da5e3e8cb057c08ad526e557c783
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2021-02-10 13:38:45 -0800

    test commit

---
+++ Tests/ContactTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactTest
+{
+    @Test
+    void constrToString()
+    {
+        Contact contact = new Contact("Harry", "Potter", "(951)453-2344", "potter@hogwarts.com");
+        String expected = "NAME: Harry Potter\nNUMBER: (951)453-2344" +
+                "\nEMAIL: potter@hogwarts.com\n-------------------------------";
+        assertEquals(expected, contact.toString());
+    }
+}
